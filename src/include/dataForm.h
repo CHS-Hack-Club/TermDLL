9927122c48931ac5b8734d78b3c241626bcef209
EzraJ <ezraprogramming@gmail.com>
2019-11-25 08:51:46 -0900

    streams

--- src/include/dataForm.h
+++ src/include/dataForm.h
@@ -4,19 +4,61 @@
 */
 #ifndef _DATAFORM_H_
 #define _DATAFORM_H_
+#include "export.h"
+#include <iostream>
 #include <vector>
 
 
-
 namespace TermDLL{
+
+	enum class inputMode{
+		zero=0, 	// Don't get any input or output
+		io, 		// Allow input and output
+		ionly, 	    // Allow NO std::cout, std::cerr, etc
+		outputonly  // Only allow output, no input
+	};
+
     template<class T>
-    class userInput{
+    class API userInput{
         public:
             userInput();
-
+			~userInput();
+			virtual T update(inputMode mode = inputMode::io, bool temp = true, bool alwaysGetInput = true, bool formatted = true){
+				T buffer;
+				switch(mode){
+					case 0:				//Zero
+						// Do nothing
+						break;
+					case 1:				//Allow input and output
+						std::cin >> buffer;
+						break;
+					case 2:				//Disable console output
+						std::streambuf* stdbuf = std::cout.rdbuf();
+						std::cout.rdbuf(NULL); // Set std::cout to output to nothing
+						if(alwaysGetInput){
+							if(formatted){
+								std::cin >> buffer;
+							}else{
+								std::getline(std::cin, buffer);
+							}
+						}
+						std::cout.rdbuf(stdbuf); // Reset to original buffer
+						break;
+					case 3:				//Only allow output
+						// Do nothing..
+						break;
+					default:
+						break;
+				}
+			};
+			virtual T data(){ // Return user input unless specified
+				return updated; 
+			};
         private:
-
-    }
+			T updated;
+			inputMode myMode = inputMode::io; // Allow input and output by default.
+    };
+    
     
 }
 
---
+++ src/include/streams.h
@@ -0,0 +1,21 @@
+/*
+	streams.h --- Contains all of the user input/output streams(think std::cout and std::cin but more customized)
+		Contains class definitions similar to std::cout and std::cin; but are customized to have formatted input
+		and formatted output, attached to the termdll library, with custom parsers.
+
+*/
+#ifndef _STREAMS_H_
+#define _STREAMS_H_
+#include "export.h"
+#include "dataForm.h"
+
+#include <iostream>
+#include <vector>
+
+
+
+
+
+
+
+#endif
